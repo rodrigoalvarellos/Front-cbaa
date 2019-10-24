ab3afd318e1b1fee1f5bd479a64bb609a0c954ed
rodrigoalvarellos <rodri.alvarellos@gmail.com>
2019-10-24 15:14:18 -0300

    test

--- src/app/@theme/layouts/one-column/one-column.layout.ts
+++ src/app/@theme/layouts/one-column/one-column.layout.ts
@@ -17,9 +17,9 @@ import { Component } from '@angular/core';
         <ng-content select="router-outlet"></ng-content>
       </nb-layout-column>
 
-      <nb-layout-footer fixed>
+      <!--<nb-layout-footer fixed>
         <ngx-footer></ngx-footer>
-      </nb-layout-footer>
+      </nb-layout-footer>-->
     </nb-layout>
   `,
 })
